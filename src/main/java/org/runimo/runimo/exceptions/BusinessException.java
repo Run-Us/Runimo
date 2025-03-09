fe09703e369fe6a200d2d22e2695735195573d53
ekgns33 <ekgns00421@gmail.com>
2025-03-09 14:25:58 +0900

    :wrench: config project

---
+++ src/main/java/org/runimo/runimo/exceptions/BusinessException.java
@@ -0,0 +1,36 @@
+package org.runimo.runimo.exceptions;
+
+import lombok.Getter;
+import org.runimo.runimo.exceptions.code.CustomResponseCode;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class BusinessException extends RuntimeException {
+
+  private final String logMessage;
+  private final CustomResponseCode errorCode;
+  private final HttpStatus httpStatusCode;
+
+  protected BusinessException(CustomResponseCode errorCode) {
+    super(errorCode.getClientMessage());
+    this.logMessage = errorCode.getLogMessage();
+    this.errorCode = errorCode;
+    this.httpStatusCode = errorCode.getHttpStatusCode();
+  }
+
+  protected BusinessException(CustomResponseCode errorCode, String logMessage) {
+    super(errorCode.getClientMessage());
+    this.logMessage = logMessage;
+    this.errorCode = errorCode;
+    this.httpStatusCode = errorCode.getHttpStatusCode();
+  }
+
+  public static BusinessException of(CustomResponseCode errorCode) {
+    return new BusinessException(errorCode, errorCode.getLogMessage());
+  }
+
+  public String getName() {
+    return this.getClass().getSimpleName();
+  }
+}
+
---
+++ src/main/java/org/runimo/runimo/exceptions/code/CustomResponseCode.java
@@ -0,0 +1,14 @@
+package org.runimo.runimo.exceptions.code;
+
+import org.springframework.http.HttpStatus;
+
+public interface CustomResponseCode {
+
+  String getCode();
+
+  String getClientMessage();
+
+  String getLogMessage();
+
+  HttpStatus getHttpStatusCode();
+}
