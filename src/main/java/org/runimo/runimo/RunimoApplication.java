65a895f0ff901a9a09537d02ce5422604b692941
ekgns33 <ekgns00421@gmail.com>
2025-03-09 14:08:18 +0900

    init
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/org/runimo/runimo/RunimoApplication.java
@@ -0,0 +1,13 @@
+package org.runimo.runimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunimoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RunimoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/runimo/runimo/RunimoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.runimo.runimo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunimoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
