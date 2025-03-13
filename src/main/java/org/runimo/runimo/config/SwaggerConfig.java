028df675ff1afcbe484884ced723dad18427be4e
ekgns33 <ekgns00421@gmail.com>
2025-03-13 16:59:31 +0900

    :sparkles: feat : swagger 설정 추가

--- src/main/java/org/runimo/runimo/config/SwaggerConfig.java
+++ src/main/java/org/runimo/runimo/config/SwaggerConfig.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -23,7 +25,14 @@ public class SwaggerConfig {
             .license(new License()
                 .name("Apache 2.0")
                 .url("https://www.apache.org/licenses/LICENSE-2.0"))
-        );
+        ).addSecurityItem(new SecurityRequirement().addList("JWT"))
+        .components(new io.swagger.v3.oas.models.Components()
+            .addSecuritySchemes("JWT",
+                new SecurityScheme()
+                    .name("JWT")
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")));
   }
 }
 
