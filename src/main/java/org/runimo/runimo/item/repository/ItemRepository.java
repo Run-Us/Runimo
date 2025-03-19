6a3846e1a1fa9ec414b131d16aca6d0411374e7a
ekgns33 <ekgns00421@gmail.com>
2025-03-19 11:24:40 +0900

    :sparkles: feat : 모든 아이템 조회 메소드 작성

--- src/main/java/org/runimo/runimo/item/repository/ItemRepository.java
+++ src/main/java/org/runimo/runimo/item/repository/ItemRepository.java
@@ -2,8 +2,14 @@ package org.runimo.runimo.item.repository;
 
 import org.runimo.runimo.item.domain.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+  @Query("select i.id from Item i")
+  List<Long> findAllItemIds();
 }
