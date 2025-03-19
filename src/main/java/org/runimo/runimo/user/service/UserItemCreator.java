8a71fe92fee086ec3dbb85009138324ea9353108
ekgns33 <ekgns00421@gmail.com>
2025-03-19 11:18:49 +0900

    :sparkles: feat : 회원가입 시 아이템 보유 엔티티 생성 로직 추가

---
+++ src/main/java/org/runimo/runimo/user/service/UserItemCreator.java
@@ -0,0 +1,36 @@
+package org.runimo.runimo.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.runimo.runimo.item.repository.ItemRepository;
+import org.runimo.runimo.user.domain.UserItem;
+import org.runimo.runimo.user.repository.UserItemRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class UserItemCreator {
+
+  private final UserItemRepository userItemRepository;
+  private final ItemRepository itemRepository;
+
+  @Transactional
+  public UserItem create(UserItem userItem) {
+    return userItemRepository.save(userItem);
+  }
+
+  /*
+  * 아이템ID, 유저ID의 모든 순서쌍을 저장한다.
+  * 회원가입 시 실행된다.
+  * */
+  @Transactional
+  public void createAll(Long userId) {
+    List<Long> itemIds = itemRepository.findAllItemIds();
+    userItemRepository.saveAll(
+        itemIds.stream()
+            .map(itemId -> new UserItem(userId, itemId, 0L))
+            .toList());
+  }
+}
