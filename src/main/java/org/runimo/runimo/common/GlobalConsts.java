e71cba2a0063d72055b64a87549b836715e605c2
ekgns33 <ekgns00421@gmail.com>
2025-03-26 20:44:01 +0900

    :recycle: refactor : 불필요한 변수 삭제, endpoint 수정사항 반영

--- src/main/java/org/runimo/runimo/common/GlobalConsts.java
+++ src/main/java/org/runimo/runimo/common/GlobalConsts.java
@@ -3,16 +3,12 @@ package org.runimo.runimo.common;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
-import java.time.ZoneId;
 import java.util.Set;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class GlobalConsts {
 
   public static final String TIME_ZONE_ID = "Asia/Seoul";
-  public static final ZoneId ZONE_ID = ZoneId.of(TIME_ZONE_ID);
-  public static final String DEFAULT_IMG_URL = "default_img_url";
-  public static final String SESSION_ATTRIBUTE_USER = "user-info";
   public static final Set<String> WHITE_LIST_ENDPOINTS = Set.of(
       "/api/v1/test/auth",
       "/api/v1/users/auth",
