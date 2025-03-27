19d842a63dff5ffbcc24d1e0902f6420b837eaf8
ekgns33 <ekgns00421@gmail.com>
2025-03-27 16:12:23 +0900

    :sparkles: feat : 유저 알지급 validate 로직 작성

--- src/main/java/org/runimo/runimo/records/repository/RecordRepository.java
+++ src/main/java/org/runimo/runimo/records/repository/RecordRepository.java
@@ -1,12 +1,28 @@
 package org.runimo.runimo.records.repository;
 
 import org.runimo.runimo.records.domain.RunningRecord;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
 public interface RecordRepository extends JpaRepository<RunningRecord, Long> {
   Optional<RunningRecord> findByRecordPublicId(String id);
+
+  @Query("SELECT r FROM RunningRecord r " +
+      "WHERE r.userId = :userId " +
+      "AND r.startedAt BETWEEN :startOfWeek AND :now " +
+      "ORDER BY r.startedAt ASC")
+  Slice<RunningRecord> findFirstRunOfWeek(
+      @Param("userId") Long userId,
+      @Param("startOfWeek") LocalDateTime startOfWeek,
+      @Param("now") LocalDateTime now,
+      Pageable pageable
+  );
 }
--- src/main/java/org/runimo/runimo/rewards/service/RewardService.java
+++ src/main/java/org/runimo/runimo/rewards/service/RewardService.java
@@ -1,7 +1,9 @@
 package org.runimo.runimo.rewards.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.runimo.runimo.item.domain.Egg;
+import org.runimo.runimo.records.domain.RunningRecord;
 import org.runimo.runimo.records.service.RecordFinder;
 import org.runimo.runimo.rewards.service.eggs.EggGrantService;
 import org.runimo.runimo.rewards.service.dtos.RewardClaimCommand;
@@ -9,9 +11,12 @@ import org.runimo.runimo.rewards.service.dtos.RewardResponse;
 import org.runimo.runimo.user.domain.User;
 import org.runimo.runimo.user.service.UserFinder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class RewardService {
@@ -20,14 +25,40 @@ public class RewardService {
   private final UserFinder userFinder;
   private final EggGrantService eggGrantService;
 
+  @Transactional
   public RewardResponse claimReward(RewardClaimCommand command) {
-    User user = userFinder.findUserById(command.userId())
+    RunningRecord runningRecord = recordFinder.findById(command.recordId())
         .orElseThrow(NoSuchElementException::new);
-    recordFinder.findById(command.recordId())
-        .orElseThrow(NoSuchElementException::new);
-    // grant egg
-    Egg grantedEgg = eggGrantService.grantRandomEggToUser(user);
+    validateRecord(runningRecord);
+    Egg grantedEgg = rewardEgg(command);
+    runningRecord.reward(command.userId());
     //TODO: 애정 보상
     return new RewardResponse(grantedEgg.getItemCode(), grantedEgg.getEggType());
+  }
+
+  private Egg rewardEgg(RewardClaimCommand command) {
+     User user = userFinder.findUserById(command.userId())
+        .orElseThrow(NoSuchElementException::new);
+     if(validateRecordIsFirstRecordOfWeek(command)) {
+        return eggGrantService.grantRandomEggToUser(user);
+     }
+     return Egg.EMPTY;
+  }
+
+  private void validateRecord(RunningRecord runningRecord) {
+    if(runningRecord.isRecordAlreadyRewarded()) throw new IllegalStateException("이미 보상이 지급되었습니다.");
+  }
+
+  private boolean validateRecordIsFirstRecordOfWeek(RewardClaimCommand command) {
+    Optional<RunningRecord> firstRecordOfWeek = recordFinder.findFirstRunOfCurrentWeek(command.userId());
+    if(firstRecordOfWeek.isEmpty()) {
+      log.info("유저 {}의 첫번째 달리기 기록이 없습니다.", command.userId());
+      return false;
+    }
+    if(!command.recordId().equals(firstRecordOfWeek.get().getId())){
+      log.info("유저 {}의 첫번째 달리기 기록이 아닙니다.", command.userId());
+      return false;
+    }
+    return true;
   }
 }
