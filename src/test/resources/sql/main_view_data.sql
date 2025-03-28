ab42e0d6c08d37154ab6a8d9ae16f32e3a9729f6
ekgns33 <ekgns00421@gmail.com>
2025-03-29 00:19:10 +0900

    :white_check_mark: test : 메인화면 테스트 코드 작성

---
+++ src/test/resources/sql/main_view_data.sql
@@ -0,0 +1,32 @@
+-- 사용자
+SET FOREIGN_KEY_CHECKS = 0;
+TRUNCATE TABLE users;
+INSERT INTO users (id, public_id, nickname, img_url, total_distance_in_meters, total_time_in_seconds, created_at,
+                   updated_at)
+VALUES (1, 'test-user-uuid-1', 'Daniel', 'https://example.com/images/user1.png', 3000, 3600, NOW(), NOW());
+SET FOREIGN_KEY_CHECKS = 1;
+
+TRUNCATE TABLE items;
+INSERT INTO items (name, item_code, description, item_type, img_url, dtype, egg_type, hatch_require_amount, created_at,
+                   updated_at)
+VALUES ('마당알', 'A100', '마당알: 기본 알', 'USABLE', 'example.url', 'EGG', 'MADANG', 10, NOW(), NOW());
+
+INSERT INTO items (name, item_code, description, item_type, img_url, dtype, egg_type, hatch_require_amount, created_at,
+                   updated_at)
+VALUES ('숲알', 'A101', '숲알: 기본 알', 'USABLE', 'example1.url', 'EGG', 'FOREST', 20, NOW(), NOW());
+
+TRUNCATE TABLE running_records;
+INSERT INTO running_records (id, user_id, record_public_id, title, started_at, end_at, total_distance, pace_in_milli_seconds, is_rewarded, created_at, updated_at)
+VALUES (1, 1, 'record-public-id-1', 'record-title-1', NOW(), NOW(), 1000, 100, false, NOW(), NOW()),
+       (2, 1, 'record-public-id-2', 'record-title-2', NOW(), NOW(), 2000, 200, false, NOW(), NOW());
+
+
+-- 보유 아이템
+TRUNCATE TABLE user_item;
+INSERT INTO user_item (id, user_id, item_id, quantity, created_at, updated_at)
+VALUES (1001, 1, 1, 2, NOW(), NOW()),
+       (1002, 1, 2, 1, NOW(), NOW());
+
+TRUNCATE TABLE user_love_point;
+INSERT INTO user_love_point (id, user_id, amount, created_at, updated_at)
+VALUES (1001, 1, 100, NOW(), NOW());
