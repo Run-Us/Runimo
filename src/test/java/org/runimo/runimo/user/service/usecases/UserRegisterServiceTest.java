7887d3c344a480aded7956f23f2df8ba26e77c12
ekgns33 <ekgns00421@gmail.com>
2025-03-29 23:41:01 +0900

    :bug: fix : `mockito.ArgumentMatchers.eq`로 인한 에러 테스트코드 수정

--- src/test/java/org/runimo/runimo/user/service/usecases/UserRegisterServiceTest.java
+++ src/test/java/org/runimo/runimo/user/service/usecases/UserRegisterServiceTest.java
@@ -15,7 +15,6 @@ import org.runimo.runimo.user.service.usecases.auth.UserRegisterService;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
 class UserRegisterServiceTest {
@@ -50,7 +49,7 @@ class UserRegisterServiceTest {
     // then
     assertNotNull(createdUser);
     verify(userCreator, times(1)).createUser(command);
-    verify(userCreator, times(1)).createUserOAuthInfo(mockUser, eq(SocialProvider.KAKAO), eq("1234"));
+    verify(userCreator, times(1)).createUserOAuthInfo(mockUser, SocialProvider.KAKAO, "1234");
     verify(userCreator, times(1)).createLovePoint(anyLong());
     verify(userItemCreator, times(1)).createAll(anyLong());
     verify(eggGrantService, times(1)).grantGreetingEggToUser(mockUser);
