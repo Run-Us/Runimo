282ab196b539fd0aedb55fe88779bcd29537b50a
ekgns33 <ekgns00421@gmail.com>
2025-04-03 15:15:06 +0900

    refactor : update field name, add swagger-spec

--- src/main/java/org/runimo/runimo/user/controller/request/RegisterEggRequest.java
+++ src/main/java/org/runimo/runimo/user/controller/request/RegisterEggRequest.java
@@ -1,6 +1,16 @@
 package org.runimo.runimo.user.controller.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(
+    description = "부화할 알 등록 요청",
+    example = """
+        {
+          "item_id": 1
+        }
+        """)
 public record RegisterEggRequest(
+    @Schema(description = "부화할 알 ID (보유중인 알)", example = "1")
     Long itemId
 ) {
 }
