f74bed4d55fd6389be25799ea4dcf676fea4bd4f
jeeheaG <jeehea99@naver.com>
2025-04-04 22:34:30 +0900

    :sparkles: add : make usecase for hatch

---
+++ src/main/java/org/runimo/runimo/hatch/service/usecase/HatchUsecase.java
@@ -0,0 +1,7 @@
+package org.runimo.runimo.hatch.service.usecase;
+
+import org.runimo.runimo.hatch.controller.dto.response.HatchEggResponse;
+
+public interface HatchUsecase {
+    HatchEggResponse execute(Long userId, Long eggId);
+}
---
+++ src/main/java/org/runimo/runimo/hatch/service/usecase/HatchUsecaseImpl.java
@@ -0,0 +1,59 @@
+package org.runimo.runimo.hatch.service.usecase;
+
+import lombok.RequiredArgsConstructor;
+import org.runimo.runimo.hatch.controller.dto.response.HatchEggResponse;
+import org.runimo.runimo.hatch.exception.HatchException;
+import org.runimo.runimo.hatch.exception.HatchHttpResponseCode;
+import org.runimo.runimo.hatch.service.HatchClient;
+import org.runimo.runimo.runimo.domain.Runimo;
+import org.runimo.runimo.runimo.domain.UserRunimo;
+import org.runimo.runimo.runimo.repository.RunimoRepository;
+import org.runimo.runimo.runimo.repository.UserRunimoRepository;
+import org.runimo.runimo.user.domain.IncubatingEgg;
+import org.runimo.runimo.user.repository.IncubatingEggRepository;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class HatchUsecaseImpl implements HatchUsecase {
+    private final RunimoRepository runimoRepository;
+    private final IncubatingEggRepository incubatingEggRepository;
+    private final UserRunimoRepository userRunimoRepository;
+    private final HatchClient hatchClient;
+
+    @Override
+    public HatchEggResponse execute(Long userId, Long eggId) {
+        IncubatingEgg incubatingEgg = incubatingEggRepository.findByEggId(eggId)
+                .orElseThrow(() -> HatchException.of(HatchHttpResponseCode.HATCH_EGG_NOT_FOUND));
+        validIncubatingEgg(incubatingEgg);
+
+        // 부화 - 러니모 획득
+        Runimo runimo = hatchClient.getRunimoFromEgg(incubatingEgg);
+
+        // 이미 보유한 러니모인지 확인
+//        Optional<UserRunimo> duplicateRunimo = userRunimoRepository.findFirstByUserIdAndRunimoCode(userId, runimo.getCode());
+
+        // 러니모 저장 -> 나중에 다른 요청으로 빼야 할 듯
+        Runimo runimoSaved = runimoRepository.save(runimo);
+        UserRunimo userRunimo = UserRunimo.builder()
+                .userId(userId)
+                .runimoId(runimoSaved.getId())
+                .build();
+        userRunimoRepository.save(userRunimo);
+
+        return new HatchEggResponse(
+                runimo.getName(),
+                runimo.getImgUrl(),
+                runimo.getCode(),
+                null
+        );
+    }
+
+    void validIncubatingEgg(IncubatingEgg incubatingEgg){
+        if(incubatingEgg.isReadyToHatch()){
+            incubatingEgg.hatch();
+        } else {
+            throw HatchException.of(HatchHttpResponseCode.HATCH_EGG_NOT_READY);
+        }
+    }
+}
