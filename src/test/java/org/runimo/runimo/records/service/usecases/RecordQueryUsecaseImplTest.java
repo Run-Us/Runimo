9d084d6283461987c9aa1d92afdcd11a4f546653
ekgns33 <ekgns00421@gmail.com>
2025-04-05 01:30:58 +0900

    :white_check_mark: test : 테스트 코드 작성

--- src/test/java/org/runimo/runimo/records/service/usecases/RecordQueryUsecaseImplTest.java
+++ src/test/java/org/runimo/runimo/records/service/usecases/RecordQueryUsecaseImplTest.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.runimo.runimo.records.service.RecordFinder;
-import org.runimo.runimo.records.service.dtos.RunningRecordDistance;
 import org.runimo.runimo.records.service.dtos.DailyStat;
 import org.runimo.runimo.records.service.dtos.WeeklyRecordStatResponse;
 import org.runimo.runimo.records.service.dtos.WeeklyStatQuery;
+import org.runimo.runimo.records.service.usecases.dtos.MonthlyRecordStatResponse;
+import org.runimo.runimo.records.service.usecases.dtos.MonthlyStatQuery;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
@@ -39,14 +40,14 @@ class RecordQueryUsecaseImplTest {
     LocalDateTime now = LocalDateTime.of(2025, 4, 6, 10, 0);
     LocalDateTime startOfWeek = now.with(DayOfWeek.MONDAY).withHour(0).withMinute(0).withSecond(0);
 
-    List<RunningRecordDistance> savedData = List.of(
-        new RunningRecordDistance(1000L, LocalDate.of(2025, 3, 31)), // 월
-        new RunningRecordDistance(2000L, LocalDate.of(2025, 4, 1)),  // 화
-        new RunningRecordDistance(3000L, LocalDate.of(2025, 4, 2)),  // 수
-        new RunningRecordDistance(4000L, LocalDate.of(2025, 4, 3)),  // 목
-        new RunningRecordDistance(5000L, LocalDate.of(2025, 4, 4)),  // 금
-        new RunningRecordDistance(6000L, LocalDate.of(2025, 4, 5)),  // 토
-        new RunningRecordDistance(7000L, LocalDate.of(2025, 4, 6))   // 일
+    List<DailyStat> savedData = List.of(
+        new DailyStat(LocalDate.of(2025, 3, 31), 1000L), // 월
+        new DailyStat(LocalDate.of(2025, 4, 1), 2000L), // 화
+        new DailyStat(LocalDate.of(2025, 4, 2), 3000L), // 수
+        new DailyStat(LocalDate.of(2025, 4, 3), 4000L), // 목
+        new DailyStat(LocalDate.of(2025, 4, 4), 5000L), // 금
+        new DailyStat(LocalDate.of(2025, 4, 5), 6000L), // 토
+        new DailyStat(LocalDate.of(2025, 4, 6), 7000L)  // 일
     );
 
     when(recordFinder.findDailyStatByUserIdBetween(eq(1L), any(LocalDateTime.class), any(LocalDateTime.class)))
@@ -79,13 +80,12 @@ class RecordQueryUsecaseImplTest {
     LocalDateTime now = LocalDateTime.of(2025, 4, 6, 10, 0);
     LocalDateTime startOfWeek = now.with(DayOfWeek.MONDAY).withHour(0).withMinute(0).withSecond(0);
 
-    List<RunningRecordDistance> savedData = List.of(
-        new RunningRecordDistance(1000L, LocalDate.of(2025, 3, 31)), // 월
-        new RunningRecordDistance(9000L, LocalDate.of(2025, 4, 1)),  // 화
-        // 수, 목 데이터 없음
-        new RunningRecordDistance(5000L, LocalDate.of(2025, 4, 4)),  // 금
-        new RunningRecordDistance(6000L, LocalDate.of(2025, 4, 5)),  // 토
-        new RunningRecordDistance(7000L, LocalDate.of(2025, 4, 6))   // 일
+    List<DailyStat> savedData = List.of(
+        new DailyStat(LocalDate.of(2025, 3, 31), 1000L), // 월
+        new DailyStat(LocalDate.of(2025, 4, 1), 9000L), // 화
+        new DailyStat(LocalDate.of(2025, 4, 4), 5000L), // 금
+        new DailyStat(LocalDate.of(2025, 4, 5), 6000L), // 토
+        new DailyStat(LocalDate.of(2025, 4, 6), 7000L)  // 일
     );
 
     when(recordFinder.findDailyStatByUserIdBetween(eq(1L), any(LocalDateTime.class), any(LocalDateTime.class)))
@@ -102,13 +102,36 @@ class RecordQueryUsecaseImplTest {
     List<DailyStat> expectedDistances = List.of(
         new DailyStat(LocalDate.of(2025, 3, 31), 1000L),
         new DailyStat(LocalDate.of(2025, 4, 1), 9000L),
-        new DailyStat(LocalDate.of(2025, 4, 2), 0L),  // 데이터 없음
-        new DailyStat(LocalDate.of(2025, 4, 3), 0L),  // 데이터 없음
         new DailyStat(LocalDate.of(2025, 4, 4), 5000L),
         new DailyStat(LocalDate.of(2025, 4, 5), 6000L),
         new DailyStat(LocalDate.of(2025, 4, 6), 7000L)
     );
 
     assertEquals(expectedDistances, response.dailyStats());
+  }
+
+  @Test
+  void 월간_일별_달리기_거리_합() {
+    // given
+    MonthlyStatQuery query = new MonthlyStatQuery(1L, 2025, 4);
+
+    List<DailyStat> savedData = List.of(
+        new DailyStat(LocalDate.of(2025, 4, 1), 2000L), // 화
+        new DailyStat(LocalDate.of(2025, 4, 2), 3000L), // 수
+        new DailyStat(LocalDate.of(2025, 4, 3), 4000L), // 목
+        new DailyStat(LocalDate.of(2025, 4, 4), 5000L), // 금
+        new DailyStat(LocalDate.of(2025, 4, 5), 6000L), // 토
+        new DailyStat(LocalDate.of(2025, 4, 6), 7000L)  // 일
+    );
+
+    when(recordFinder.findDailyStatByUserIdBetween(eq(1L), any(LocalDateTime.class), any(LocalDateTime.class)))
+        .thenReturn(savedData);
+
+    // when
+    MonthlyRecordStatResponse res = queryUsecase.getUserMonthlyRecordStat(query);
+
+    // then
+    assertEquals(savedData.size(), res.dailyStats().size());
+    assertEquals(2000L, res.dailyStats().getFirst().distance());
   }
 }
