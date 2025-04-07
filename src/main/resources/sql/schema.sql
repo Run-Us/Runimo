c3a9f184b3a65403a00f1b79d606931caf023676
jeeheaG <jeehea99@naver.com>
2025-04-07 17:32:24 +0900

    :sparkles: add : add SQL for insert runimo_definition static data

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -161,3 +161,11 @@ ALTER TABLE `user_token`
 
 ALTER TABLE `oauth_account`
     ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+
+-- insert static data
+INSERT INTO runimo_definition (id, name, code, description, img_url, egg_type, created_at, updated_at)
+VALUES (1, '토끼', 'R-101', '마당 토끼예여', 'http://dummy1', 'MADANG', NOW(), NOW()),
+       (2, '강아지', 'R-102', '마당 강아지예여', 'http://dummy2', 'MADANG', NOW(), NOW()),
+       (3, '오리', 'R-103', '마당 오리예여', 'http://dummy3', 'MADANG', NOW(), NOW()),
+       (4, '늑대', 'R-104', '주인이 다른 마당 늑대예여', 'http://dummy4', 'MADANG', NOW(), NOW());
