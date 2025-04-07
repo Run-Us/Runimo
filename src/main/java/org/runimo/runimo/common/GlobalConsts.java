e5e67aab38f7a9715734317eb364cbfb733a6e60
ekgns33 <ekgns00421@gmail.com>
2025-04-07 14:29:10 +0900

    :sparkles: feat : endpoint 변경

--- src/main/java/org/runimo/runimo/common/GlobalConsts.java
+++ src/main/java/org/runimo/runimo/common/GlobalConsts.java
@@ -12,7 +12,7 @@ public final class GlobalConsts {
   public static final String TIME_ZONE_ID = "Asia/Seoul";
   public static final Set<String> WHITE_LIST_ENDPOINTS = Set.of(
       "/api/v1/test/auth",
-      "/api/v1/users/auth",
+      "/api/v1/auth",
       "/swagger-ui",
       "/v3/api-docs"
   );
