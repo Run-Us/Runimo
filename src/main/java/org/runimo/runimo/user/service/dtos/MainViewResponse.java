61fdc5443b29bc04f298c7bd082f24863be26f52
ekgns33 <ekgns00421@gmail.com>
2025-04-08 18:55:43 +0900

    :memo: chore : add swagger-ui schema info

--- src/main/java/org/runimo/runimo/user/service/dtos/MainViewResponse.java
+++ src/main/java/org/runimo/runimo/user/service/dtos/MainViewResponse.java
@@ -1,9 +1,13 @@
 package org.runimo.runimo.user.service.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.runimo.runimo.runimo.service.model.MainRunimoStat;
 
+@Schema(description = "메인화면 응답")
 public record MainViewResponse(
+    @Schema(description = "대표 러니모 정보 - 등록안하면 Null")
     MainRunimoStat mainRunimoStatNullable,
+    @Schema(description = "사용자 정보")
     UserMainViewInfo userInfo
 ) {
 
