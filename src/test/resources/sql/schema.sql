891fdaff0e98ff9b57ee95b02f71312e202c8986
ekgns33 <ekgns00421@gmail.com>
2025-04-08 22:25:15 +0900

    :hammer: chore : update ddl script for `users`

--- src/test/resources/sql/schema.sql
+++ src/test/resources/sql/schema.sql
@@ -21,11 +21,12 @@ CREATE TABLE `users`
     `public_id`                VARCHAR(255),
     `nickname`                 VARCHAR(255),
     `img_url`                  VARCHAR(255),
-    `total_distance_in_meters` BIGINT NOT NULL DEFAULT 0,
-    `total_time_in_seconds`    BIGINT NOT NULL DEFAULT 0,
+    `total_distance_in_meters` BIGINT    NOT NULL DEFAULT 0,
+    `total_time_in_seconds`    BIGINT    NOT NULL DEFAULT 0,
     `main_runimo_id`           BIGINT,
-    `updated_at`               TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `created_at`               TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `gender`                   VARCHAR(24),
+    `updated_at`               TIMESTAMP          DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `created_at`               TIMESTAMP          DEFAULT CURRENT_TIMESTAMP,
     `deleted_at`               TIMESTAMP NULL
 );
 
@@ -35,25 +36,25 @@ CREATE TABLE `user_token`
     `device_token` VARCHAR(255) NOT NULL,
     `created_at`   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     `updated_at`   TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `deleted_at`   TIMESTAMP NULL,
+    `deleted_at`   TIMESTAMP    NULL,
 
     FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
 );
 
 CREATE TABLE `user_love_point`
 (
-    `id`        BIGINT PRIMARY KEY AUTO_INCREMENT,
-    `user_id`   BIGINT NOT NULL,
-    `amount`    BIGINT NOT NULL DEFAULT 0,
-    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `id`         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    `user_id`    BIGINT    NOT NULL,
+    `amount`     BIGINT    NOT NULL DEFAULT 0,
+    `created_at` TIMESTAMP          DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP          DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     `deleted_at` TIMESTAMP NULL
 );
 
 CREATE TABLE `oauth_account`
 (
     `id`          BIGINT PRIMARY KEY AUTO_INCREMENT,
-    `user_id`     BIGINT NOT NULL,
+    `user_id`     BIGINT    NOT NULL,
     `provider`    VARCHAR(255),
     `provider_id` VARCHAR(255),
     `created_at`  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -66,7 +67,7 @@ CREATE TABLE `oauth_account`
 CREATE TABLE `running_record`
 (
     `id`                    INTEGER PRIMARY KEY AUTO_INCREMENT,
-    `user_id`               INTEGER NOT NULL,
+    `user_id`               INTEGER      NOT NULL,
     `record_public_id`      VARCHAR(255) NOT NULL,
     `title`                 VARCHAR(255),
     `started_at`            TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -77,7 +78,7 @@ CREATE TABLE `running_record`
     `pace_per_km`           VARCHAR(10000),
     `created_at`            TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     `updated_at`            TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `deleted_at`            TIMESTAMP NULL
+    `deleted_at`            TIMESTAMP    NULL
 );
 
 CREATE TABLE `item`
@@ -93,27 +94,27 @@ CREATE TABLE `item`
     `hatch_require_amount` BIGINT,
     `created_at`           TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     `updated_at`           TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `deleted_at`           TIMESTAMP NULL
+    `deleted_at`           TIMESTAMP    NULL
 );
 
 CREATE TABLE `item_activity`
 (
     `id`                  INTEGER PRIMARY KEY AUTO_INCREMENT,
-    `activity_user_id`    INTEGER NOT NULL,
-    `activity_item_id`    INTEGER NOT NULL,
+    `activity_user_id`    INTEGER      NOT NULL,
+    `activity_item_id`    INTEGER      NOT NULL,
     `activity_event_type` VARCHAR(255) NOT NULL,
     `quantity`            INTEGER,
     `created_at`          TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     `updated_at`          TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `deleted_at`          TIMESTAMP NULL
+    `deleted_at`          TIMESTAMP    NULL
 );
 
 CREATE TABLE `user_item`
 (
     `id`         BIGINT PRIMARY KEY AUTO_INCREMENT,
-    `user_id`    INTEGER NOT NULL,
-    `item_id`    INTEGER NOT NULL,
-    `quantity`   INTEGER NOT NULL,
+    `user_id`    INTEGER   NOT NULL,
+    `item_id`    INTEGER   NOT NULL,
+    `quantity`   INTEGER   NOT NULL,
     `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     `deleted_at` TIMESTAMP NULL
@@ -122,8 +123,8 @@ CREATE TABLE `user_item`
 CREATE TABLE `incubating_egg`
 (
     `id`                        BIGINT PRIMARY KEY AUTO_INCREMENT,
-    `user_id`                   INTEGER NOT NULL,
-    `egg_id`                    INTEGER NOT NULL,
+    `user_id`                   INTEGER   NOT NULL,
+    `egg_id`                    INTEGER   NOT NULL,
     `current_love_point_amount` INTEGER,
     `hatch_require_amount`      INTEGER,
     `egg_status`                VARCHAR(255),
@@ -134,27 +135,27 @@ CREATE TABLE `incubating_egg`
 
 CREATE TABLE `runimo_definition`
 (
-    `id`            BIGINT PRIMARY KEY AUTO_INCREMENT,
-    `name`          VARCHAR(255),
-    `code`          VARCHAR(255),
-    `description`   VARCHAR(255),
-    `img_url`       varchar(255),
-    `egg_type`      varchar(255) NOT NULL,
-    `created_at`    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    `updated_at`    TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `deleted_at`    TIMESTAMP NULL
+    `id`          BIGINT PRIMARY KEY AUTO_INCREMENT,
+    `name`        VARCHAR(255),
+    `code`        VARCHAR(255),
+    `description` VARCHAR(255),
+    `img_url`     varchar(255),
+    `egg_type`    varchar(255) NOT NULL,
+    `created_at`  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`  TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `deleted_at`  TIMESTAMP    NULL
 );
 
 CREATE TABLE `runimo`
 (
-    `id`         BIGINT PRIMARY KEY AUTO_INCREMENT,
-    `user_id`    BIGINT NOT NULL,
-    `runimo_definition_id`  BIGINT NOT NULL,
-    `total_run_count` BIGINT NOT NULL DEFAULT 0,
-    `total_distance_in_meters` BIGINT NOT NULL DEFAULT 0,
-    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `deleted_at` TIMESTAMP NULL
+    `id`                       BIGINT PRIMARY KEY AUTO_INCREMENT,
+    `user_id`                  BIGINT    NOT NULL,
+    `runimo_definition_id`     BIGINT    NOT NULL,
+    `total_run_count`          BIGINT    NOT NULL DEFAULT 0,
+    `total_distance_in_meters` BIGINT    NOT NULL DEFAULT 0,
+    `created_at`               TIMESTAMP          DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`               TIMESTAMP          DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `deleted_at`               TIMESTAMP NULL
 );
 
 ALTER TABLE `user_token`
