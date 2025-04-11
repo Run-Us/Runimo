2890a9b47dffcc7819a536b43c59b91d3112b3be
ekgns33 <ekgns00421@gmail.com>
2025-04-11 12:05:24 +0900

    :white_check_mark: test : withdraw acceptance test

---
+++ src/test/java/org/runimo/runimo/user/api/UserWithdrawAcceptanceTest.java
@@ -0,0 +1,76 @@
+package org.runimo.runimo.user.api;
+
+import static io.restassured.RestAssured.given;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.runimo.runimo.CleanUpUtil;
+import org.runimo.runimo.auth.jwt.JwtTokenFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+class UserWithdrawAcceptanceTest {
+
+    @LocalServerPort
+    int port;
+
+    @Autowired
+    private JwtTokenFactory jwtTokenFactory;
+
+    @Autowired
+    private CleanUpUtil cleanUpUtil;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+
+    @AfterEach
+    void tearDown() {
+        cleanUpUtil.cleanUpUserInfos();
+    }
+
+
+    @Test
+    @Sql(scripts = "/sql/user_mypage_test_data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    void 회원_탈퇴_성공_시_유저_조회_불가() {
+
+        String token = "Bearer " + jwtTokenFactory.generateAccessToken("test-user-uuid-1");
+
+        given()
+            .header("Authorization", token)
+            .when()
+            .get("/api/v1/main")
+            .then()
+            .statusCode(HttpStatus.OK.value());
+
+        given()
+            .header("Authorization", token)
+            .contentType(ContentType.JSON)
+
+            .when()
+            .delete("/api/v1/users")
+
+            .then()
+            .log().all()
+            .statusCode(204);
+
+        given()
+            .header("Authorization", token)
+            .when()
+            .get("/api/v1/main")
+            .then()
+            .statusCode(HttpStatus.NOT_FOUND.value());
+
+    }
+
+}
