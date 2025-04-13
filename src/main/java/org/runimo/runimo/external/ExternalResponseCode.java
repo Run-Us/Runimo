887681b7ba3cc9b332b02e8fc5651a69180f7ad6
ekgns33 <ekgns00421@gmail.com>
2025-04-13 23:49:41 +0900

    :bug: fix : fix response code fields

--- src/main/java/org/runimo/runimo/external/ExternalResponseCode.java
+++ src/main/java/org/runimo/runimo/external/ExternalResponseCode.java
@@ -4,21 +4,23 @@ import org.runimo.runimo.exceptions.code.CustomResponseCode;
 import org.springframework.http.HttpStatus;
 
 public enum ExternalResponseCode implements CustomResponseCode {
-    PRESIGNED_URL_FETCHED(HttpStatus.CREATED, "Presigned URL 발급 성공", "Presigned URL 발급 성공");
+    PRESIGNED_URL_FETCHED("ESH2011", HttpStatus.CREATED, "Presigned URL 발급 성공", "Presigned URL 발급 성공");
 
-    private final HttpStatus code;
+    private final String code;
+    private final HttpStatus httpStatus;
     private final String clientMessage;
     private final String logMessage;
 
-    ExternalResponseCode(HttpStatus httpStatus, String clientMessage, String logMessage) {
-        this.code = httpStatus;
+    ExternalResponseCode(String code, HttpStatus httpStatus, String clientMessage, String logMessage) {
+        this.code = code;
+        this.httpStatus = httpStatus;
         this.clientMessage = clientMessage;
         this.logMessage = logMessage;
     }
 
     @Override
     public String getCode() {
-        return this.name();
+        return this.code;
     }
 
     @Override
@@ -33,6 +35,6 @@ public enum ExternalResponseCode implements CustomResponseCode {
 
     @Override
     public HttpStatus getHttpStatusCode() {
-        return this.code;
+        return this.httpStatus;
     }
 }
