ec552f49b833447319f251495fe48f77cd67580b
ekgns33 <ekgns00421@gmail.com>
2025-04-15 00:36:24 +0900

    :sparkles: feat : set random uuid as signup token subject

--- src/main/java/org/runimo/runimo/auth/jwt/SignupTokenPayload.java
+++ src/main/java/org/runimo/runimo/auth/jwt/SignupTokenPayload.java
@@ -2,7 +2,8 @@ package org.runimo.runimo.auth.jwt;
 
 import org.runimo.runimo.user.domain.SocialProvider;
 
-public record RegisterTokenPayload(
+public record SignupTokenPayload(
+    String token,
     String providerId,
     SocialProvider socialProvider
 ) {
