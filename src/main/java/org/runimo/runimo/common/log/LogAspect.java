29078be96c0dc1fd75ec49836a641656f3803f17
jeeheaG <jeehea99@naver.com>
2025-04-18 03:43:58 +0900

    :sparkles: add : make LogAspect class

---
+++ src/main/java/org/runimo/runimo/common/log/LogAspect.java
@@ -0,0 +1,83 @@
+package org.runimo.runimo.common.log;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.runimo.runimo.common.log.model.HttpRequestLogInfo;
+import org.runimo.runimo.common.log.model.MethodEndLogInfo;
+import org.runimo.runimo.common.log.model.MethodStartLogInfo;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Aspect
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class LogAspect {
+
+    private final LogMessageFormatter logMessageFormatter;
+
+    @Pointcut("execution(* org.runimo.runimo..controller.*Controller.*(..))")
+    private void controller() {
+    }
+
+    @Pointcut("@annotation(org.runimo.runimo.common.log.ServiceLog)")
+    private void service() {
+    }
+
+    @Before("controller()")
+    public void apiRequestLogger() {
+        ServletRequestAttributes attributes = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes());
+        if (attributes == null) {
+            log.info("ServletRequestAttributes is null");
+            return;
+        }
+
+        HttpServletRequest request = attributes.getRequest();
+        HttpRequestLogInfo logInfo = HttpRequestLogInfo.of(request);
+
+        log.info(logMessageFormatter.toHttpRequestLogMessage(logInfo));
+    }
+
+    @Around("controller() || service()")
+    public Object calledMethodLogger(ProceedingJoinPoint pjp) throws Throwable {
+        MethodStartLogInfo methodStartLogInfo = getMethodStartLogInfo(pjp);
+        log.info(logMessageFormatter.toMethodStartLogMessage(methodStartLogInfo));
+
+        long startTime = getCurrentTimeMillis();
+        Object proceedReturn = pjp.proceed();
+        long endTime = getCurrentTimeMillis();
+
+        MethodEndLogInfo methodEndLogInfo = MethodEndLogInfo.of(pjp, endTime - startTime,
+            proceedReturn);
+        log.info(logMessageFormatter.toMethodEndLogMessage(methodEndLogInfo));
+
+        return proceedReturn;
+    }
+
+    private static MethodStartLogInfo getMethodStartLogInfo(ProceedingJoinPoint pjp) {
+        MethodStartLogInfo methodStartLogInfo;
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            methodStartLogInfo = MethodStartLogInfo.of(pjp, true, authentication.getName());
+        } else {
+            methodStartLogInfo = MethodStartLogInfo.of(pjp, false, null);
+        }
+
+        return methodStartLogInfo;
+    }
+
+    private long getCurrentTimeMillis() {
+        return System.currentTimeMillis();
+    }
+
+}
