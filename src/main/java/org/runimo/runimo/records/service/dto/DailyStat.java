bc85495f1755626b2259e1548b8796f269906538
ekgns33 <ekgns00421@gmail.com>
2025-04-30 20:32:43 +0900

    :sparkles: feat : update query to return RecordStatDto list

--- src/main/java/org/runimo/runimo/records/service/dto/DailyStat.java
+++ src/main/java/org/runimo/runimo/records/service/dto/DailyStat.java
@@ -1,12 +1,36 @@
 package org.runimo.runimo.records.service.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import lombok.Getter;
 
-public record DailyStat(
-    LocalDate date,
-    Long distance
-) {
+@Getter
+public class DailyStat {
 
+    private LocalDate date;
+    @JsonIgnore
+    private Long timeInSeconds;
+    @JsonIgnore
+    private Long runningCount;
+    private Long distanceInMeters;
+
+    public DailyStat(LocalDate date, Long timeInSeconds, Long runningCount, Long distanceInMeters) {
+        this.date = date;
+        this.timeInSeconds = timeInSeconds;
+        this.runningCount = runningCount;
+        this.distanceInMeters = distanceInMeters;
+    }
+
+    public void addData(RecordStatDto recordStat) {
+        this.timeInSeconds += recordStat.getStartedAt().until(recordStat.getEndAt(), ChronoUnit.SECONDS);
+        this.runningCount += 1;
+        this.distanceInMeters += recordStat.getRunningDistanceInMeters();
+    }
+
+    public static DailyStat empty(LocalDate date) {
+        return new DailyStat(date, 0L, 0L, 0L);
+    }
 }
 
