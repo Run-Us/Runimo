5f3e5aaf8fd76ccb1a7c2a6732ec59c71346766f
ekgns33 <ekgns00421@gmail.com>
2025-05-07 15:43:28 +0900

    :sparkles: feat : create RefreshToken entity

---
+++ src/main/java/org/runimo/runimo/auth/repository/RefreshTokenJpaRepository.java
@@ -0,0 +1,18 @@
+package org.runimo.runimo.auth.repository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import org.runimo.runimo.auth.domain.RefreshToken;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+@Profile({"prod", "dev"})
+public interface RefreshTokenJpaRepository extends JpaRepository<RefreshToken, String> {
+
+  Optional<RefreshToken> findByUserId(Long id);
+
+  @Query("select distinct r from RefreshToken r " +
+      "where r.userId = :userId and r.updatedAt > :cutOffDateTime")
+  Optional<RefreshToken> findByUserIdAfterCutoffTime(Long userId, LocalDateTime cutOffDateTime);
+}
