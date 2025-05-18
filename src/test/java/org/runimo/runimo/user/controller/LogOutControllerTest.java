f227816f05357319183367ffacf0928772da7174
jeeheaG <jeehea99@naver.com>
2025-05-19 01:29:20 +0900

    :white_check_mark: test : add test for log out api

---
+++ src/test/java/org/runimo/runimo/user/controller/LogOutControllerTest.java
@@ -0,0 +1,69 @@
+package org.runimo.runimo.user.controller;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.runimo.runimo.TestConsts.TEST_USER_UUID;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.runimo.runimo.CleanUpUtil;
+import org.runimo.runimo.TokenUtils;
+import org.runimo.runimo.exceptions.code.CustomResponseCode;
+import org.runimo.runimo.user.enums.UserHttpResponseCode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+class LogOutControllerTest {
+
+    @LocalServerPort
+    int port;
+
+    @Autowired
+    private CleanUpUtil cleanUpUtil;
+
+    @Autowired
+    private TokenUtils tokenUtils;
+
+    private String token;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+        token = tokenUtils.createTokenByUserPublicId(TEST_USER_UUID);
+    }
+
+    @AfterEach
+    void tearDown() {
+        cleanUpUtil.cleanUpUserInfos();
+    }
+
+    @Test
+    @Sql(scripts = "/sql/log_out_test_data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    void logOut() {
+        CustomResponseCode responseCode = UserHttpResponseCode.LOG_OUT_SUCCESS;
+
+        given()
+            .header("Authorization", token)
+            .contentType(ContentType.JSON)
+
+            .when()
+            .post("/api/v1/users/log-out")
+
+            .then()
+            .log().all()
+            .statusCode(responseCode.getHttpStatusCode().value())
+
+            .body("code", equalTo(responseCode.getCode()))
+            .body("message", equalTo(responseCode.getClientMessage()));
+    }
+}
---
+++ src/test/resources/sql/log_out_test_data.sql
@@ -0,0 +1,7 @@
+-- 사용자
+SET FOREIGN_KEY_CHECKS = 0;
+TRUNCATE TABLE users;
+INSERT INTO users (id, public_id, nickname, img_url, total_distance_in_meters, total_time_in_seconds, created_at,
+                   updated_at)
+VALUES (1, 'test-user-uuid-1', 'Daniel', 'https://example.com/images/user1.png', 10000, 3600, NOW(), NOW());
+SET FOREIGN_KEY_CHECKS = 1;
