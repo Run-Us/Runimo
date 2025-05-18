1d613e3855b0b9cd41170dde4d060012a343dd55
ekgns33 <ekgns00421@gmail.com>
2025-05-18 18:12:28 +0900

    :white_check_mark: test : add unit test

--- src/test/java/org/runimo/runimo/auth/service/SignUpUsecaseTest.java
+++ src/test/java/org/runimo/runimo/auth/service/SignUpUsecaseTest.java
@@ -1,5 +1,7 @@
 package org.runimo.runimo.auth.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
@@ -17,8 +19,12 @@ import org.runimo.runimo.auth.jwt.JwtResolver;
 import org.runimo.runimo.auth.jwt.JwtTokenFactory;
 import org.runimo.runimo.auth.jwt.SignupTokenPayload;
 import org.runimo.runimo.auth.repository.SignupTokenRepository;
+import org.runimo.runimo.auth.service.dto.SignupUserResponse;
 import org.runimo.runimo.auth.service.dto.UserSignupCommand;
 import org.runimo.runimo.external.FileStorageService;
+import org.runimo.runimo.item.EggFixtures;
+import org.runimo.runimo.rewards.service.eggs.EggGrantService;
+import org.runimo.runimo.user.UserFixtures;
 import org.runimo.runimo.user.domain.Gender;
 import org.runimo.runimo.user.domain.SocialProvider;
 import org.runimo.runimo.user.enums.UserHttpResponseCode;
@@ -27,53 +33,84 @@ import org.runimo.runimo.user.service.UserRegisterService;
 
 class SignUpUsecaseTest {
 
-  @Mock
-  private UserRegisterService userRegisterService;
-  @Mock
-  private FileStorageService fileStorageService;
-  @Mock
-  private JwtTokenFactory jwtTokenFactory;
-  @Mock
-  private SignupTokenRepository signupTokenRepository;
-  @Mock
-  private AppleUserTokenRepository appleUserTokenRepository;
-  @Mock
-  private JwtResolver jwtResolver;
+    @Mock
+    private UserRegisterService userRegisterService;
+    @Mock
+    private FileStorageService fileStorageService;
+    @Mock
+    private EggGrantService eggGrantService;
+    @Mock
+    private JwtTokenFactory jwtTokenFactory;
+    @Mock
+    private SignupTokenRepository signupTokenRepository;
+    @Mock
+    private AppleUserTokenRepository appleUserTokenRepository;
+    @Mock
+    private JwtResolver jwtResolver;
 
-  private SignUpUsecase sut;
+    private SignUpUsecase sut;
 
-  @BeforeEach
-  void setUp() {
-    MockitoAnnotations.openMocks(this);
-    sut = new SignUpUsecaseImpl(
-        userRegisterService,
-        fileStorageService,
-        jwtTokenFactory,
-        signupTokenRepository,
-        appleUserTokenRepository,
-        jwtResolver
-    );
-  }
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        sut = new SignUpUsecaseImpl(
+            userRegisterService,
+            fileStorageService,
+            eggGrantService,
+            jwtTokenFactory,
+            signupTokenRepository,
+            appleUserTokenRepository,
+            jwtResolver
+        );
+    }
 
-  @Test
-  void 유저가_이미_존재하면_실패() {
-    // given
-    String registerToken = "dummy.token.value";
-    SignupTokenPayload payload = new SignupTokenPayload(registerToken, "socialId123",
-        SocialProvider.KAKAO);
+    @Test
+    void 회원가입_시_환영_알_지급() {
+        // given
+        String registerToken = "dummy.token.value";
+        SignupTokenPayload payload = new SignupTokenPayload(registerToken, "socialId123",
+            SocialProvider.KAKAO);
 
-    when(jwtResolver.getSignupTokenPayload(registerToken)).thenReturn(payload);
-    when(signupTokenRepository.findByIdAndCreatedAtAfter(eq(registerToken), any()))
-        .thenReturn(Optional.of(new SignupToken(
-            registerToken, "refresh", "refresh", SocialProvider.KAKAO)));
+        when(jwtResolver.getSignupTokenPayload(registerToken)).thenReturn(payload);
+        when(signupTokenRepository.findByIdAndCreatedAtAfter(eq(registerToken), any()))
+            .thenReturn(Optional.of(new SignupToken(
+                registerToken, "refresh", "refresh", SocialProvider.KAKAO)));
+        when(userRegisterService.registerUser(any())).thenReturn(UserFixtures.getUserWithId(1L));
+        when(eggGrantService.grantGreetingEggToUser(any())).thenReturn(
+            EggFixtures.createDefaultEgg());
+        when(jwtTokenFactory.generateTokenPair(any())).thenReturn(UserFixtures.TEST_TOKEN_PAIR);
 
-    doThrow(new org.runimo.runimo.auth.exceptions.SignUpException(
-        UserHttpResponseCode.SIGNIN_FAIL_ALREADY_EXIST))
-        .when(userRegisterService)
-        .validateExistingUser(payload.providerId(), payload.socialProvider());
+        SignupUserResponse response = sut
+            .register(new UserSignupCommand(registerToken, "nickname", null, Gender.UNKNOWN));
 
-    assertThrows(SignUpException.class, () -> {
-      sut.register(new UserSignupCommand(registerToken, "nickname", null, Gender.UNKNOWN));
-    });
-  }
+        assertEquals(1L, response.userId());
+        assertEquals(UserFixtures.TEST_USER_NICKNAME, response.nickname());
+        assertEquals(UserFixtures.TEST_USER_IMG_URL, response.imgUrl());
+        assertNotNull(response.tokenPair());
+        assertEquals(EggFixtures.TEST_EGG_NAME, response.greetingEggName());
+        assertEquals(EggFixtures.TEST_EGG_TYPE_NAME, response.greetingEggType());
+        assertEquals(EggFixtures.TEST_EGG_IMG_URL, response.greetingEggImgUrl());
+    }
+
+    @Test
+    void 유저가_이미_존재하면_실패() {
+        // given
+        String registerToken = "dummy.token.value";
+        SignupTokenPayload payload = new SignupTokenPayload(registerToken, "socialId123",
+            SocialProvider.KAKAO);
+
+        when(jwtResolver.getSignupTokenPayload(registerToken)).thenReturn(payload);
+        when(signupTokenRepository.findByIdAndCreatedAtAfter(eq(registerToken), any()))
+            .thenReturn(Optional.of(new SignupToken(
+                registerToken, "refresh", "refresh", SocialProvider.KAKAO)));
+
+        doThrow(new org.runimo.runimo.auth.exceptions.SignUpException(
+            UserHttpResponseCode.SIGNIN_FAIL_ALREADY_EXIST))
+            .when(userRegisterService)
+            .validateExistingUser(payload.providerId(), payload.socialProvider());
+
+        assertThrows(SignUpException.class, () -> {
+            sut.register(new UserSignupCommand(registerToken, "nickname", null, Gender.UNKNOWN));
+        });
+    }
 }
