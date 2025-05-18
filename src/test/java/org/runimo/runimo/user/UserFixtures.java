0682201e217970058b2485a2ea5ff8f861974ca5
ekgns33 <ekgns00421@gmail.com>
2025-05-18 18:11:40 +0900

    :white_check_mark: test : update userfixtures

--- src/test/java/org/runimo/runimo/user/UserFixtures.java
+++ src/test/java/org/runimo/runimo/user/UserFixtures.java
@@ -1,14 +1,21 @@
 package org.runimo.runimo.user;
 
+import org.runimo.runimo.auth.service.dto.TokenPair;
 import org.runimo.runimo.user.domain.User;
 import org.springframework.test.util.ReflectionTestUtils;
 
 public final class UserFixtures {
 
+    public static final String TEST_USER_NICKNAME = "test";
+    public static final String TEST_USER_IMG_URL = "test";
+    public static final TokenPair TEST_TOKEN_PAIR = new TokenPair(
+        "test-access-token-1", "test-refresh-token-1"
+    );
+
     public static User getDefaultUser() {
         return User.builder()
-            .nickname("test")
-            .imgUrl("test")
+            .nickname(TEST_USER_NICKNAME)
+            .imgUrl(TEST_USER_IMG_URL)
             .totalDistanceInMeters(0L)
             .totalTimeInSeconds(0L)
             .build();
@@ -16,8 +23,8 @@ public final class UserFixtures {
 
     public static User getUserWithId(Long id) {
         User user = User.builder()
-            .nickname("test")
-            .imgUrl("test")
+            .nickname(TEST_USER_NICKNAME)
+            .imgUrl(TEST_USER_NICKNAME)
             .totalDistanceInMeters(0L)
             .totalTimeInSeconds(0L)
             .build();
