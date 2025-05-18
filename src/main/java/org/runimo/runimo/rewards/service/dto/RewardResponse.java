b68b76b62777da2f12d77fc38b7ced1b85e4362d
ekgns33 <ekgns00421@gmail.com>
2025-05-18 17:13:34 +0900

    :sparkles: feat : update reward-response

--- src/main/java/org/runimo/runimo/rewards/service/dto/RewardResponse.java
+++ src/main/java/org/runimo/runimo/rewards/service/dto/RewardResponse.java
@@ -1,11 +1,24 @@
 package org.runimo.runimo.rewards.service.dto;
 
 
+import org.runimo.runimo.item.domain.Egg;
+
 public record RewardResponse(
     Boolean isRewarded,
     String eggCode,
     String eggType,
+    String eggImgUrl,
     Long lovePointAmount
 ) {
+
+    public static RewardResponse of(Egg egg, Long lovePointAmount) {
+        return new RewardResponse(
+            egg != Egg.EMPTY,
+            egg.getItemCode(),
+            egg.getName(),
+            egg.getImgUrl(),
+            lovePointAmount
+        );
+    }
 
 }
