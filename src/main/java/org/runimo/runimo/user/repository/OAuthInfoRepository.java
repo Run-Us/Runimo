444c74cffd3552cbd90824e0d88fa3d67117d05d
Dahoon Moon(Daniel Moon) <76658405+ekgns33@users.noreply.github.com>
2025-06-15 12:04:40 +0900

    :bug: fix : add not-null contraint to user existence query (#100)

--- src/main/java/org/runimo/runimo/user/repository/OAuthInfoRepository.java
+++ src/main/java/org/runimo/runimo/user/repository/OAuthInfoRepository.java
@@ -1,6 +1,5 @@
 package org.runimo.runimo.user.repository;
 
-import jakarta.validation.constraints.NotNull;
 import java.util.Optional;
 import org.runimo.runimo.user.domain.OAuthInfo;
 import org.runimo.runimo.user.domain.SocialProvider;
@@ -12,20 +11,20 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface OAuthInfoRepository extends JpaRepository<OAuthInfo, Long> {
 
-  @Query("""
-        SELECT o FROM OAuthInfo o
-                WHERE o.deletedAt is null AND o.provider = :provider AND o.providerId = :providerId
-      """)
-  Optional<OAuthInfo> findByProviderAndProviderId(SocialProvider provider, String providerId);
+    @Query("""
+          SELECT o FROM OAuthInfo o
+                  WHERE o.deletedAt is null AND o.provider = :provider AND o.providerId = :providerId
+        """)
+    Optional<OAuthInfo> findByProviderAndProviderId(SocialProvider provider, String providerId);
 
-  SocialProvider user(User user);
+    SocialProvider user(User user);
 
-  @Query("""
-      select o from OAuthInfo o 
-              where o.deletedAt is null AND o.user.id = :userId
-      """)
-  Optional<OAuthInfo> findByUserId(Long userId);
+    @Query("""
+        select o from OAuthInfo o 
+         where o.deletedAt is null AND o.user.id = :userId
+        """)
+    Optional<OAuthInfo> findByUserId(Long userId);
 
-  boolean existsByProviderIdAndProvider(@NotNull String providerId,
-      @NotNull SocialProvider socialProvider);
+    boolean existsByProviderIdAndProviderAndDeletedAtIsNull(String providerId,
+        SocialProvider socialProvider);
 }
