a4c309c9090bd26fa23d0b3746c7a17fd4193201
ekgns33 <ekgns00421@gmail.com>
2025-07-07 18:16:00 +0900

    :sparkles: feat : add fields to update request

--- src/main/java/org/runimo/runimo/user/controller/request/UpdateNotificationAllowedRequst.java
+++ src/main/java/org/runimo/runimo/user/controller/request/UpdateNotificationAllowedRequst.java
@@ -1,7 +1,13 @@
 package org.runimo.runimo.user.controller.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "알림 허용 여부 업데이트 요청")
 public record UpdateNotificationAllowedRequst(
-    boolean allowed
+    @Schema(description = "알림 허용 여부", example = "true")
+    boolean allowed,
+    String deviceToken,
+    String platform
 ) {
 
 }
