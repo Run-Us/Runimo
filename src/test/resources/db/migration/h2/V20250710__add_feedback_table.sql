30f67f7e34b91b80c958f80a8f8151f73e5b1dc6
ekgns33 <ekgns00421@gmail.com>
2025-07-10 00:55:39 +0900

    :bug: fix : add default timestamp value to migration script

--- src/test/resources/db/migration/h2/V20250710__add_feedback_table.sql
+++ src/test/resources/db/migration/h2/V20250710__add_feedback_table.sql
@@ -1,9 +1,9 @@
-create table `user_feedback`
+CREATE TABLE `user_feedback`
 (
     `id`         BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `user_id`    BIGINT NOT NULL,
     `rate`       INT,
     `content`    VARCHAR(128),
-    `created_at` TIMESTAMP,
-    `updated_at` TIMESTAMP
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
