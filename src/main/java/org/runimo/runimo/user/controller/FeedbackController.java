b539a813fbc1be666345b3d25921039f3e038434
ekgns33 <ekgns00421@gmail.com>
2025-07-10 00:59:48 +0900

    :sparkles: feat : add swagger annotations

--- src/main/java/org/runimo/runimo/user/controller/FeedbackController.java
+++ src/main/java/org/runimo/runimo/user/controller/FeedbackController.java
@@ -1,5 +1,9 @@
 package org.runimo.runimo.user.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.runimo.runimo.common.response.SuccessResponse;
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "피드백 API")
 @RestController
 @RequestMapping("/api/v1/feedback")
 @RequiredArgsConstructor
@@ -20,6 +25,13 @@ public class FeedbackController {
 
     private final FeedbackUsecase feedbackUsecase;
 
+    @Operation(summary = "피드백 생성", description = "사용자가 피드백을 작성합니다.")
+    @ApiResponses(
+        value = {
+            @ApiResponse(responseCode = "201", description = "평가 생성 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청 데이터")
+        }
+    )
     @PostMapping
     public ResponseEntity<SuccessResponse<Void>> createFeedback(
         @UserId Long userId,
